560f5c67205bb872b48ee38413df641492510540
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-05-29 19:15:01 -0400

    added an acounts object to the created persist folder

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ console.log('Starting PASSWORD MANAGER');
 let storage = require('node-persist');
 storage.initSync();
 
-storage.setItemSync('name', 'Seth');
+// storage.setItemSync('accounts', [{
+//   username: 'Seth',
+//   balance: 0
+// }]);
 
-let name = storage.getItemSync('name');
-console.log('Saved name is: ' + name);
+let accounts = storage.getItemSync('accounts');
+console.log(accounts);
