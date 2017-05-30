66a8cf2dfe4958792bc2d75000f7a573d9be86fc
Administrator <administrator@Administrators-MacBook-Pro.local>
2017-05-29 20:32:53 -0400

    edited app.js

--- app.js
+++ app.js
@@ -1,61 +1,56 @@
-console.log('Starting PASSWORD MANAGER');
+console.log("Starting PASSWORD MANAGER");
 
-let storage = require('node-persist');
+let storage = require("node-persist");
 storage.initSync();
 
 let sethAccount = {
-  name: 'Google',
-  username: 'sethbrady',
-  password: '1234'
+  name: "Google",
+  username: "sethbrady",
+  password: "1234"
 };
 
 let JuliaAccount = {
-  name: 'Yahoo',
-  username: 'js1221',
-  password: '1221'
+  name: "Yahoo",
+  username: "js1221",
+  password: "1221"
 };
 
 let catAccount = {
-  name: 'Tinder',
-  username: 'Kat',
-  password: 'meow'
+  name: "Tinder",
+  username: "Kat",
+  password: "meow"
 };
 
-function createAccount(account){
-  let accounts = storage.getItemSync('accounts');
+function createAccount(account) {
+  let accounts = storage.getItemSync("accounts");
 
-  if(accounts === undefined){
+  if (accounts === undefined) {
     accounts = [];
   }
 
   accounts.push(account);
-  storage.setItemSync('accounts', accounts);
+  storage.setItemSync("accounts", accounts);
 
   return account;
 }
 
 function getAccount(accountName) {
-
-  let accounts = storage.getItemSync('accounts');
+  let accounts = storage.getItemSync("accounts");
   let matchAccount;
 
-  accounts.forEach(function(account){
-    if (account.name === accountName){
+  accounts.forEach(function(account) {
+    if (account.name === accountName) {
       matchAccount = account;
     }
   });
   return matchAccount;
 }
 
-// createAccount(sethAccount);
-// createAccount(JuliaAccount);
-// createAccount(catAccount);
-
-let googleAccount = getAccount('Google');
+let googleAccount = getAccount("Google");
 console.log(googleAccount);
 
-let yahooAccount = getAccount('Yahoo');
+let yahooAccount = getAccount("Yahoo");
 console.log(yahooAccount);
 
-let tinderAccount = getAccount('Tinder');
+let tinderAccount = getAccount("Tinder");
 console.log(tinderAccount);
